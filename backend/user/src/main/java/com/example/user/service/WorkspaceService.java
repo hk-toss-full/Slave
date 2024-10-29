98bf5042099bc5d2bfb307671ce983a3664ddfed
cho <inbrain144@gmail.com>
2024-10-29 13:21:00 +0900

    fix : workspace 부분 수정

--- backend/user/src/main/java/com/example/user/service/WorkspaceService.java
+++ backend/user/src/main/java/com/example/user/service/WorkspaceService.java
@@ -20,12 +20,6 @@ public class WorkspaceService {
     private final UserWorkspaceAccessRepository userWorkspaceAccessRepository;
     private final UserRepository userRepository;
 
-    public List<Workspace> getUserWorkspacesByEmail(String email) {
-        Long userId = userRepository.findUserIdByUserEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 이메일입니다."));
-        return getUserWorkspaces(userId); // userId로 워크스페이스 목록 조회
-    }
-
     public List<Workspace> getUserWorkspaces(Long userId) {
         return workspaceRepository.findAllByUserId(userId); // 유저가 접근 가능한 워크스페이스 리스트 반환
     }
